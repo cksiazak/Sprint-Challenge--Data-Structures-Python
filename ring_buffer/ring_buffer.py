2938856739ceea5d10284836c95f378dc8977035
Christopher Ksiazak <cksiazak@gmail.com>
2020-05-29 19:10:17 -0600

    completed ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,20 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.length = 0
+        self.storage = []
+        self.index = 0
 
     def append(self, item):
-        pass
+        if self.length >= self.capacity:
+            self.storage[self.index] = item
+            if self.index < self.capacity - 1:
+                self.index += 1
+            else:
+                self.index = 0
+        else:
+            self.storage.append(item)
+            self.length += 1
 
     def get(self):
-        pass
+        return self.storage
